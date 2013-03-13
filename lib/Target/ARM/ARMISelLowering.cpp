45d9ea11da5b7e8dc1cd6ce6e8cd1653553e2165
ILyoan <ilyoan@gmail.com>
2013-03-13 11:42:08 +0900

    Splitstack implementation for android

--- lib/Target/ARM/ARMISelLowering.cpp
+++ lib/Target/ARM/ARMISelLowering.cpp
@@ -3112,6 +3112,8 @@ ARMTargetLowering::LowerFormalArguments(SDValue Chain,
     VarArgStyleRegisters(CCInfo, DAG, dl, Chain,
                          CCInfo.getNextStackOffset());
 
+  AFI->setArgumentStackSize(CCInfo.getNextStackOffset());
+
   return Chain;
 }
 
