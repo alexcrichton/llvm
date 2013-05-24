1cb5671ff85ea6d6888df5a7242b4e5aca15ed97
Brian Anderson <banderson@mozilla.com>
2013-05-23 18:57:10 -0700

    Add MIPS segmented stacks

--- lib/Target/Mips/MipsISelLowering.cpp
+++ lib/Target/Mips/MipsISelLowering.cpp
@@ -2730,6 +2730,8 @@ MipsTargetLowering::LowerFormalArguments(SDValue Chain,
                         &OutChains[0], OutChains.size());
   }
 
+  MipsFI->setArgumentStackSize(CCInfo.getNextStackOffset());
+
   return Chain;
 }
 
